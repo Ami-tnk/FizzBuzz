ff7904adea7457b5392794a38c03b30d044320d7
Ami-tnk <ami1y251992@gmail.com>
2021-03-05 14:02:14 +0900

    [Finish] fizz_buzz.rb

--- greeting.rb
+++ greeting.rb
@@ -1,5 +1,6 @@
-def greeting
-  'Hello'
+def greeting(name)      # メソッド(引数)
+  return "Hello, #{name}!"     # returnで戻り値指定
+  "Good morning, #{name}!"
 end
 
-puts greeting
+puts greeting('John')   # 'John'を引数として渡す
